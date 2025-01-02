2581f4277a092b444377836cc542a3b9096eeed0
HarshMane2604 <maneharsh72@gmail.com>
2025-01-03 01:34:11 +0530

    Project is created

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const fs = require('fs');
-const { log } = require('console');
+// const { log } = require('console');
+// const { console } = require('inspector');
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -17,6 +18,25 @@ app.get('/', (req, res) => {
     });
     
 });
+
+app.get('/files/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, 'utf-8', function(err, data) {
+        if (err) throw err;
+        res.render("show", {filename: req.params.filename, filedata: data});
+        
+    });
+});
+
+app.post('/create', (req, res) => {
+    // console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+        res.redirect('/');
+    });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
